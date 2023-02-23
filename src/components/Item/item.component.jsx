7a25b93d33bca5a246c9835667fa6d3a0ca19ae1
toan2013dn <tofbest267@gmail.com>
2023-02-24 01:15:45 +0700

    initial commit

---
+++ src/components/Item/item.component.jsx
@@ -0,0 +1,11 @@
+import PartnershipIcon from "@/assets/icons/partnership.svg";
+
+const Item = ({ imageUrl }) => {
+  return (
+    <div>
+        <img src={PartnershipIcon} alt="dog" />
+    </div>
+  );
+};
+
+export default Item;
