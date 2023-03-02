aad9162aa5827813fa82e84a38eb12f391398d55
toan2013dn <tofbest267@gmail.com>
2023-03-02 15:00:47 +0700

    fix navigation and add links

--- src/components/Navigation/navigation.component.jsx
+++ src/components/Navigation/navigation.component.jsx
@@ -2,17 +2,20 @@ import "./navigation.component.scss";
 import { ReactComponent as Shopping } from "@/assets/icons/Shopping-icon.svg";
 import { ReactComponent as User } from "@/assets/icons/User.svg";
 import { ReactComponent as Wishlist } from "@/assets/icons/Wishlist.svg";
+import Badge from '@mui/material/Badge';
 
 function Navigation() {
   return (
     <div className="navigation">
       <div className="navigation-item">
+      <Badge badgeContent={4} color="primary">
         <Shopping className="navigation-item--icon" />
-        <div className="navigation-item--text">2</div>
+       </Badge>
       </div>
       <div className="navigation-item">
+      <Badge badgeContent={4} color="primary">
         <Wishlist className="navigation-item--icon" />
-        <div className="navigation-item--text">2</div>
+       </Badge>
       </div>
       <div className="navigation-item">
         <User className="navigation-item--icon" />
