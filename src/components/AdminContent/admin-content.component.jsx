885f9ee8daa08dbdab6d31d40cc9d122fcb34c42
toan2013dn <tofbest267@gmail.com>
2023-03-15 20:59:24 +0700

    admin page categories management layout

--- src/components/AdminContent/admin-content.component.jsx
+++ src/components/AdminContent/admin-content.component.jsx
@@ -1,12 +1,18 @@
 import './admin-content.styles.scss'
+import CategoriesManagement from './CategoriesManagement/categories-management.component'
 import OrderManagementTable from './OrderManagement/order-management-table.component'
 
 function AdminContent() {
   return (
     <div className="admin-content">
-      <div className="order-table">
+      {/* <div className="order-table">
         <h3>Quản Lý Đơn Hàng</h3>
         <OrderManagementTable />
+      </div> */}
+
+      <div className="categories">
+        <h3>Quản Lý Thể Loại</h3>
+        <CategoriesManagement />
       </div>
     </div>
   )
