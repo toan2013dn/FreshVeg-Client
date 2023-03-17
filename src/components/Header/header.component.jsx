ef49e86f46bb1e2deac7878bf5072ad9c7f48073
unknown <tofbest267@gmail.com>
2023-03-18 01:27:25 +0700

    login/signup button styles & popup login

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -5,16 +5,17 @@ import { useNavigate } from 'react-router-dom'
 import Menu from '../Menu/menu.component'
 import Navigation from '../Navigation/navigation.component'
 import SearchBar from '../SearchBar/searchbar.component'
+import Login from '@/pages/Login/login.page'
+import Modal from '@mui/material/Modal'
 
 import { useUserStore } from '@/store'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
   const [userInfo, setUserInfo] = useUserStore((state) => [state.userInfo, state.setUserInfo])
   const navigate = useNavigate()
-
-  const handleClickLogin = () => {
-    navigate('/login')
-  }
 
   const handleClickSignUp = () => {
     navigate('/signup')
@@ -35,11 +36,16 @@ const Header = () => {
           <button className="register-btn btn-style" onClick={handleClickSignUp}>
             Đăng Ký
           </button>
-          <button className="login-btn btn-style" onClick={handleClickLogin}>
+          <button className="login-btn btn-style" onClick={() => setIsOpenModal(true)}>
             Đăng Nhập
           </button>
         </div>
       )}
+      <Modal open={isOpenModal} onClose={() => setIsOpenModal(false)}>
+        <div>
+          <Login onClose={() => setIsOpenModal(false)} />
+        </div>
+      </Modal>
     </header>
   )
 }
--- src/pages/Login/login.page.jsx
+++ src/pages/Login/login.page.jsx
@@ -1,10 +1,11 @@
-import "./login.page.scss";
-import { ReactComponent as Logo } from "@/assets/icons/logo.svg";
-import LoginForm from "@/components/LoginForm/loginform.component";
-import SocialLogin from "@/components/SocialLogin/socialLogin.component";
-import { Link } from "react-router-dom";
+import './login.page.scss'
+import { ReactComponent as Logo } from '@/assets/icons/logo.svg'
+import LoginForm from '@/components/LoginForm/loginform.component'
+import SocialLogin from '@/components/SocialLogin/socialLogin.component'
+import CloseIcon from '@mui/icons-material/Close'
+import { Link } from 'react-router-dom'
 
-function Login() {
+function Login({ onClose }) {
   return (
     <div className="login">
       <Logo to="/" className="logo" />
@@ -13,14 +14,14 @@ function Login() {
         <span>or continue with</span>
         <SocialLogin />
       </div>
-
       <div className="register">
         <span>
-          Bạn không có tài khoản? <Link to={"/signup"}>Đăng Kí</Link>
+          Bạn không có tài khoản? <Link to={'/signup'}>Đăng Kí</Link>
         </span>
       </div>
+      <CloseIcon className="close-icon" onClick={onClose} />
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
