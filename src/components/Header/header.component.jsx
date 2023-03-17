5f3c83a85db9783be2c8eb0ad245168bd8fafb6d
unknown <tofbest267@gmail.com>
2023-03-17 20:29:41 +0700

    login & signup btn styles

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,39 +1,47 @@
 import './header.styles.scss'
-import { ReactComponent as Logo } from "@/assets/icons/logo.svg";
-import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Logo } from '@/assets/icons/logo.svg'
+import { useNavigate } from 'react-router-dom'
 
-import Menu from "../Menu/menu.component";
-import Navigation from "../Navigation/navigation.component";
-import SearchBar from "../SearchBar/searchbar.component";
+import Menu from '../Menu/menu.component'
+import Navigation from '../Navigation/navigation.component'
+import SearchBar from '../SearchBar/searchbar.component'
 
-import { useUserStore } from '@/store';
+import { useUserStore } from '@/store'
 
 const Header = () => {
-  const [userInfo, setUserInfo] = useUserStore((state) => [
-    state.userInfo,
-    state.setUserInfo
-  ])
+  const [userInfo, setUserInfo] = useUserStore((state) => [state.userInfo, state.setUserInfo])
   const navigate = useNavigate()
 
   const handleClickLogin = () => {
     navigate('/login')
   }
 
+  const handleClickSignUp = () => {
+    navigate('/signup')
+  }
+
   return (
     <header className="header">
-        <Logo to="/" className='logo' />
+      <Logo to="/" className="logo" />
 
-        <Menu />
+      <Menu />
 
-        <SearchBar />
+      <SearchBar />
 
-        {
-          userInfo ? <Navigation />  : <button onClick={handleClickLogin}>Đăng nhập</button>
-        }
-
+      {userInfo ? (
+        <Navigation />
+      ) : (
+        <div className="button">
+          <button className="register-btn btn-style" onClick={handleClickSignUp}>
+            Đăng Ký
+          </button>
+          <button className="login-btn btn-style" onClick={handleClickLogin}>
+            Đăng Nhập
+          </button>
+        </div>
+      )}
     </header>
-  );
-};
+  )
+}
 
-export default Header;
-
+export default Header
