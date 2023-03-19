d91e738c6ff9cfcc8a221a0edf9d1bc36f5292ed
unknown <tofbest267@gmail.com>
2023-03-19 21:40:34 +0700

    validation sign-up form

--- src/components/SignupForm/signupForm.component.jsx
+++ src/components/SignupForm/signupForm.component.jsx
@@ -1,41 +1,136 @@
-import "./signupForm.component.scss";
-import React, { useState } from "react";
-import { ReactComponent as UserSignup } from "@/assets/icons/UserSignup.svg";
-import { ReactComponent as Gmail } from "@/assets/icons/Gmail.svg";
-import { ReactComponent as Password } from "@/assets/icons/Password.svg";
+import './signupForm.component.scss'
+import React, { useState } from 'react'
+import { ReactComponent as UserSignup } from '@/assets/icons/UserSignup.svg'
+import { ReactComponent as Gmail } from '@/assets/icons/Gmail.svg'
+import { ReactComponent as Password } from '@/assets/icons/Password.svg'
+
+import { ToastContainer, toast } from 'react-toastify'
+// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+
+import ShowPassword from '@mui/icons-material/Visibility'
+import HiddenPassword from '@mui/icons-material/VisibilityOff'
 
 function SignupForm() {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userName, setUserName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [errors, setErrors] = useState({})
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword)
+  }
 
   const handleInputChange = (e) => {
-    const { id, value } = e.target;
+    const { id, value } = e.target
 
-    if (id === "userName") {
-      setUserName(value);
+    if (id === 'userName') {
+      setUserName(value)
     }
-    if (id === "email") {
-      setEmail(value);
+    if (id === 'email') {
+      setEmail(value)
     }
-    if (id === "password") {
-      setPassword(value);
+    if (id === 'password') {
+      setPassword(value)
     }
-    if (id === "confirmPassword") {
-      setConfirmPassword(value);
+    if (id === 'confirmPassword') {
+      setConfirmPassword(value)
     }
-  };
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    if (validateForm()) {
+      const code = '12345'
+      Swal.fire({
+        html: `<h4>Hãy nhập mã xác nhận được gửi đến ${email}</h4>`,
+        input: 'text',
+        showCancelButton: true,
+        confirmButtonText: 'Xác nhận',
+        cancelButtonText: 'Hủy',
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Vui lòng nhập mã xác nhận!'
+          } else if (value !== code) {
+            return 'Mã xác nhận không đúng!'
+          }
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = '/'
+        }
+      })
+    }
+  }
+
+  const validateForm = () => {
+    let formIsValid = true
+
+    if (!userName) {
+      formIsValid = false
+      setErrors((errors) => ({ ...errors, userName: 'Vui lòng nhập tên người dùng!' }))
+    } else {
+      const userNameRegex = /^[a-zA-Z0-9]+$/
+      if (!userNameRegex.test(userName)) {
+        formIsValid = false
+        setErrors((errors) => ({ ...errors, userName: 'Vui lòng nhập tên người dùng hợp lệ!' }))
+      } else {
+        setErrors((errors) => ({ ...errors, userName: '' }))
+      }
+    }
+
+    if (!email) {
+      formIsValid = false
+      setErrors((errors) => ({ ...errors, email: 'Vui lòng nhập email!' }))
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if (!emailRegex.test(email)) {
+        formIsValid = false
+        setErrors((errors) => ({ ...errors, email: 'Vui lòng nhập email hợp lệ!' }))
+      } else {
+        setErrors((errors) => ({ ...errors, email: '' }))
+      }
+    }
+
+    if (!password) {
+      formIsValid = false
+      setErrors((errors) => ({ ...errors, password: 'Vui lòng nhập mật khẩu!' }))
+    } else {
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+      if (!passwordRegex.test(password)) {
+        formIsValid = false
+        setErrors((errors) => ({
+          ...errors,
+          password: 'Nhập mật khẩu có tối thiểu: 6 ký tự bao gồm số, chữ cái thường và hoa.',
+        }))
+      } else {
+        setErrors((errors) => ({ ...errors, password: '' }))
+      }
+    }
+
+    if (!confirmPassword) {
+      formIsValid = false
+      setErrors((errors) => ({ ...errors, confirmPassword: 'Vui lòng nhập lại mật khẩu!' }))
+    } else if (password !== confirmPassword) {
+      formIsValid = false
+      setErrors((errors) => ({ ...errors, confirmPassword: 'Mật khẩu không khớp!' }))
+    } else {
+      setErrors((errors) => ({ ...errors, confirmPassword: '' }))
+    }
+
+    return formIsValid
+  }
 
   return (
     <div className="signup-form">
       <form onSubmit={handleSubmit}>
         <div className="userName form-input">
-          <label className="form-label" for="userName">
+          <label className="form-label" htmlFor="userName">
             User Name
           </label>
           <UserSignup />
@@ -46,52 +141,56 @@ function SignupForm() {
             onChange={(e) => handleInputChange(e)}
             placeholder="Enter your User Name"
           />
+          {errors['userName'] !== '' && <span className="error">{errors['userName']}</span>}
         </div>
         <div className="email form-input">
-          <label className="form-label" for="email">
+          <label className="form-label" htmlFor="email">
             Email
           </label>
           <Gmail />
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => handleInputChange(e)}
-            placeholder="Email"
-          />
+          <input type="email" id="email" value={email} onChange={(e) => handleInputChange(e)} placeholder="Email" />
+          {errors['email'] !== '' && <span className="error">{errors['email']}</span>}
         </div>
         <div className="password form-input">
-          <label className="form-label" for="password">
+          <label className="form-label" htmlFor="password">
             Password
           </label>
           <Password />
           <input
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             id="password"
             value={password}
             onChange={(e) => handleInputChange(e)}
             placeholder="Password"
           />
+          {errors['password'] && <span className="error">{errors['password']}</span>}
+          <button type="button" className="button-toggle" onClick={toggleShowPassword}>
+            {showPassword ? <ShowPassword /> : <HiddenPassword />}
+          </button>
         </div>
-        <div className="confirm-password form-input">
-          <label className="form-label" for="confirmPassword">
+        <div className="confirm-password form-input password">
+          <label className="form-label" htmlFor="confirmPassword">
             Confirm Password
           </label>
           <Password />
           <input
-            type="password"
+            type={showConfirmPassword ? 'text' : 'password'}
             id="confirmPassword"
             value={confirmPassword}
             onChange={(e) => handleInputChange(e)}
             placeholder="Confirm Password"
           />
+          {errors['confirmPassword'] && <span className="error">{errors['confirmPassword']}</span>}
+          <button type="button" className="button-toggle" onClick={toggleShowConfirmPassword}>
+            {showConfirmPassword ? <ShowPassword /> : <HiddenPassword />}
+          </button>
         </div>
-        <button onClick={() => handleSubmit()} type="submit" class="btn">
+        <button className="register-btn" onClick={(e) => handleSubmit(e)} type="submit">
           Register
         </button>
       </form>
     </div>
-  );
+  )
 }
 
-export default SignupForm;
+export default SignupForm
--- src/pages/SignUp/signup.page.jsx
+++ src/pages/SignUp/signup.page.jsx
@@ -1,33 +1,32 @@
-import "./signup.page.scss";
-import { ReactComponent as Logo } from "@/assets/icons/logo.svg";
-import { Link } from "react-router-dom";
-import Register from "@/assets/images/Register.webp";
-import SignupForm from "@/components/SignupForm/signupForm.component";
+import './signup.page.scss'
+import { ReactComponent as Logo } from '@/assets/icons/logo.svg'
+import { Link } from 'react-router-dom'
+import Register from '@/assets/images/Register.webp'
+import SignupForm from '@/components/SignupForm/signupForm.component'
 
 function SignUp() {
   return (
-    <div className="signup wrapper" style={{maxHeight: "900px"}}>
-      <div className="signup-image" >
-        {/* setHeight img bi bop nho anh */}
-        <img style={{maxHeight: "900px"}}  src={Register} alt="signup" />
+    <div className="signup" style={{ maxHeight: '100vh' }}>
+      <div className="signup-image">
+        <img style={{ maxHeight: '100vh' }} src={Register} alt="signup" />
       </div>
       <div className="signup-content">
-          <Logo className="img"></Logo>
+        <Logo className="img"></Logo>
         <div className="flex">
           <h3>Đăng Kí</h3>
           <h4>Nếu bạn đã có sẵn tài khoản</h4>
           <h4>
             Bạn có thể
-            <span>
-              <Link to={"/login"}>Đăng Nhập</Link>
-            </span>
+            <button>
+              <Link to={'/login'}>Đăng Nhập</Link>
+            </button>
           </h4>
         </div>
 
         <SignupForm />
       </div>
     </div>
-  );
+  )
 }
 
-export default SignUp;
+export default SignUp
