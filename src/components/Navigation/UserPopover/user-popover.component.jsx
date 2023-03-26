9fc37fc20675b027941fe653be9e9de08406e848
unknown <tofbest267@gmail.com>
2023-03-27 01:06:05 +0700

    feat: user popover

---
+++ src/components/Navigation/UserPopover/user-popover.component.jsx
@@ -0,0 +1,77 @@
+import './user-popover.styles.scss'
+
+import * as React from 'react'
+import Popover from '@mui/material/Popover'
+import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined'
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
+
+import { useUserStore } from '@/store'
+import { ReactComponent as User } from '@/assets/icons/User.svg'
+
+function UserPopover() {
+  const [userInfo, setUserInfo] = useUserStore((state) => [state.userInfo, state.setUserInfo])
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'simple-popover' : undefined
+
+  return (
+    <div>
+      <div className="user-popover--img" aria-describedby={id} variant="contained" onClick={handleClick}>
+        {userInfo.avatar ? <img src={userInfo.avatar} alt="avatar" /> : <User className="navigation-item--icon" />}
+      </div>
+
+      {/* <User className="navigation-item--icon" aria-describedby={id} variant="contained" onClick={handleClick} /> */}
+      <Popover
+        className="user-popover"
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        PaperProps={{
+          style: { width: '200px', marginTop: '15px' },
+        }}
+      >
+        <div className="user-popover--content">
+          <div className="user-popover--content-info">
+            <p>{userInfo.name}</p>
+            <p>{userInfo.email}</p>
+          </div>
+          <div className="option-btn">
+            <button>
+              <BadgeOutlinedIcon />
+              <p>Trang cá nhân</p>
+            </button>
+            <button>
+              <LocalMallOutlinedIcon />
+              <p>Lịch sử đơn hàng</p>
+            </button>
+            <button>
+              <LogoutOutlinedIcon />
+              <p>Đăng xuất</p>
+            </button>
+          </div>
+        </div>
+      </Popover>
+    </div>
+  )
+}
+
+export default UserPopover
