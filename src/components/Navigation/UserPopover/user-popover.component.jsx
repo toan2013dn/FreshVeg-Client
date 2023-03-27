a8c61ee7cafbdecebcfec78ba0a9a7277deba5ce
unknown <tofbest267@gmail.com>
2023-03-27 12:18:14 +0700

    feat: click away disable poper

--- src/components/Navigation/UserPopover/user-popover.component.jsx
+++ src/components/Navigation/UserPopover/user-popover.component.jsx
@@ -1,12 +1,13 @@
 import './user-popover.styles.scss'
 
 import * as React from 'react'
-import { useState } from 'react'
 import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined'
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined'
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 import Popper from '@mui/material/Popper'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useUserStore } from '@/store'
 import { ReactComponent as User } from '@/assets/icons/User.svg'
@@ -34,6 +35,9 @@ function UserPopover() {
   const handleClick = (event) => {
     setAnchorEl(anchorEl ? null : event.currentTarget)
   }
+  const handleClickAway = () => {
+    setAnchorEl(null)
+  }
 
   const open = Boolean(anchorEl)
   const id = open ? 'simple-popper' : undefined
@@ -43,7 +47,6 @@ function UserPopover() {
       <div className="user-popover--img" aria-describedby={id} variant="contained" onClick={handleClick}>
         {userInfo.avatar ? <img src={userInfo.avatar} alt="avatar" /> : <User className="navigation-item--icon" />}
       </div>
-
       <Popper
         id={id}
         open={open}
@@ -74,28 +77,30 @@ function UserPopover() {
           },
         ]}
       >
-        <div className="user-popover--content">
-          <div className="user-popover--content-info">
-            <h4 style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '150px' }}>
-              {userInfo.name}
-            </h4>
-            <h4>{userInfo.email}</h4>
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <div className="user-popover--content">
+            <div className="user-popover--content-info">
+              <h4 style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '150px' }}>
+                {userInfo.name}
+              </h4>
+              <h4>{userInfo.email}</h4>
+            </div>
+            <div className="option-btn">
+              <button onClick={handleProfile}>
+                <BadgeOutlinedIcon />
+                <h4>Trang cá nhân</h4>
+              </button>
+              <button>
+                <LocalMallOutlinedIcon />
+                <h4>Lịch sử đơn hàng</h4>
+              </button>
+              <button onClick={handleLogout}>
+                <LogoutOutlinedIcon />
+                <h4>Đăng xuất</h4>
+              </button>
+            </div>
           </div>
-          <div className="option-btn">
-            <button onClick={handleProfile}>
-              <BadgeOutlinedIcon />
-              <h4>Trang cá nhân</h4>
-            </button>
-            <button>
-              <LocalMallOutlinedIcon />
-              <h4>Lịch sử đơn hàng</h4>
-            </button>
-            <button onClick={handleLogout}>
-              <LogoutOutlinedIcon />
-              <h4>Đăng xuất</h4>
-            </button>
-          </div>
-        </div>
+        </ClickAwayListener>
       </Popper>
     </div>
   )
