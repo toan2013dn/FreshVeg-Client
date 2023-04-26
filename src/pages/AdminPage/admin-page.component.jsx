3fd1fce2d26ffe180b22a403f3af88cb1d308d74
Tran Toan <tofbest267@gmail.com>
2023-04-26 21:51:29 +0700

    feat: loading admin page

--- src/pages/AdminPage/admin-page.component.jsx
+++ src/pages/AdminPage/admin-page.component.jsx
@@ -1,19 +1,34 @@
 import './admin-page.styles.scss'
 
-import AdminHeader from '@/components/AdminHeader/admin-header.component'
-import AdminContent from '../../components/AdminContent/admin-content.component'
-import AdminSideBar from '@/components/AdminHeader/AdminSidebar/admin-sidebar.component'
+import { useEffect, useState } from 'react'
+
 import AdminNavbar from '@/components/AdminHeader/AdminNavbar/admin-navbar.component'
+import AdminSideBar from '@/components/AdminHeader/AdminSidebar/admin-sidebar.component'
+import ReactLoading from 'react-loading'
+import AdminContent from '../../components/AdminContent/admin-content.component'
 
 function AdminPage() {
-  return (
-    <div className="admin-page">
-      {/* <AdminHeader /> */}
+  const [loading, setLoading] = useState(false)
 
-      <AdminNavbar />
-      <AdminSideBar />
-      <AdminContent />
-    </div>
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2300)
+  }, [])
+
+  return (
+    <>
+      {loading ? (
+        <ReactLoading className="loading" type={'spin'} color={'#4caf50'} height={'6%'} width={'6%'} />
+      ) : (
+        <div className="admin-page">
+          <AdminNavbar />
+          <AdminSideBar />
+          <AdminContent />
+        </div>
+      )}
+    </>
   )
 }
 
